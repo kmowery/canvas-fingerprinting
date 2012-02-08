0693b8cb1c7fd7fbb723bec0da3f96815f3ce8e7
kmowery <kmowery@gmail.com>
2012-02-08 13:15:30 -0800

    Start work on a webl-gl experiment

--- experiments.rb
+++ experiments.rb
@@ -6,3 +6,8 @@ dev.name = "dev"
 dev.scripts = ["/experiments/canvas-text/run.js"]
 dev.save
 
+webgl_teapot = Experiment.find_or_create_by_name('webgl-teapot')
+webgl_teapot.name = "webgl-teapot"
+webgl_teapot.scripts = ["/experiments/webgl-teapot/webgl.js"]
+webgl_teapot.save
+
---
+++ static/experiments/webgl-teapot/webgl.js
@@ -0,0 +1,11 @@
+
+
+
+
+$(document).ready(function() {
+  var x = $("scratch");
+
+  $("#scratch").width(250);
+  $("#scratch").height(250);
+});
+
