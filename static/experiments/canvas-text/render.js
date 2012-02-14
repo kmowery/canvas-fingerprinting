dbc385c6c1c222b72ff815f55eb962c85319c842
kmowery <kmowery@gmail.com>
2012-02-14 14:20:39 -0800

    results page improved slightly

--- static/experiments/canvas-text/render.js
+++ static/experiments/canvas-text/render.js
@@ -23,3 +23,35 @@ function renderdiff(elementid, pixels1, pixels2) {
   context.putImageData(imageData,0,0);
 }
 
+
+function setPixelAt(data, n) {
+  var loc = Math.floor(parseInt(n) / 4) * 4;
+
+  data[ loc ] = 0;
+  data[ loc +1 ] = 0;
+  data[ loc +2 ] = 0;
+  data[ loc +3 ] = 255;
+
+  return data;
+}
+
+function renderdiffmap(elementid, pixels1, pixels2) {
+  var canvas = document.getElementById(elementid);
+  var context = canvas.getContext("2d");
+  var imageData = context.getImageData(0,0, canvas.width, canvas.height);
+
+  for(var p in pixels1) {
+    if( !(p in pixels2) || (pixels1[p] !== pixels2[p])) {
+      imageData.data = setPixelAt(imageData.data, p);
+    }
+  }
+
+  for(var p in pixels2) {
+    if( !(p in pixels1) || (pixels1[p] !== pixels2[p])) {
+      imageData.data= setPixelAt(imageData.data, p);
+    }
+  }
+
+  context.putImageData(imageData,0,0);
+}
+
