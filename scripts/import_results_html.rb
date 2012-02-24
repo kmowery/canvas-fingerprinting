281312705648245411de3233728b419916d2c1da
kmowery <kmowery@gmail.com>
2012-02-24 02:09:53 -0800

    html import script is now aware of userids

--- scripts/import_results_html.rb
+++ scripts/import_results_html.rb
@@ -17,36 +17,46 @@ rows = []
 
 #entries = html.css('div#header').each {|x|
 #entries = html.xpath('//span[starts-with(@id, "long_answer")]').each {|x|
-html.css('span[id^="answer_long"]').each {|span|
-  # Why is there a <dl>? I don't know.
-  dl = span.children[0]
+html.css('tr').each {|row|
 
-  row = {}
-  dl.css("dt").each {|c|
-    row[c.inner_html.gsub(":","")] = c.next_sibling.next_sibling.inner_html
+  row.css('span[id^="answer_long"]').each {|span|
+
+    # If we find an answer in this row, check for links that are IDs
+    userid = row.css("a").first.children.first.to_s.strip
+
+    # Why is there a <dl>? I don't know.
+    dl = span.children[0]
+
+    row = {}
+    dl.css("dt").each {|c|
+      row[c.inner_html.gsub(":","")] = c.next_sibling.next_sibling.inner_html
+    }
+
+    s = Sample.new
+    s.useragent = row["useragent"]
+    s.userinput = row["input"]
+    s.webglrenderer = row["renderer"]
+    s.webglversion = row["webglversion"]
+    s.webglvendor = row["webglvendor"]
+    s.save
+
+    puts "Browser unclear: " + s.useragent if s.browser.nil?
+    puts "OS unclear: " + s.useragent if s.os.nil?
+
+    row.keys.select{ |k|  k =~ /exp-/ }.sort.each do |experiment_name|
+      exp = Experiment.where(:name => experiment_name.gsub("exp-","")).first
+      c     = Canvas.new
+
+      c    .experiment_id = exp.id
+      c    .sample_id = s.id
+      c    .png = row[experiment_name]
+
+      if /exp-webgl-teapot/ =~ experiment_name and c.png != "no webgl" then
+        puts userid
+      end
+
+      c.save
+    end
   }
-
-  s = Sample.new
-  s.useragent = row["useragent"]
-  s.userinput = row["input"]
-  s.webglrenderer = row["renderer"]
-  s.webglversion = row["webglversion"]
-  s.webglvendor = row["webglvendor"]
-  s.save
-
-  pp row if /Safari/ =~ s.browser
-
-  puts "Browser unclear: " + s.useragent if s.browser.nil?
-  puts "OS unclear: " + s.useragent if s.os.nil?
-
-  row.keys.select{ |k|  k =~ /exp-/ }.sort.each do |experiment_name|
-    exp = Experiment.where(:name => experiment_name.gsub("exp-","")).first
-    c     = Canvas.new
-
-    c    .experiment_id = exp.id
-    c    .sample_id = s.id
-    c    .png = row[experiment_name]
-    c.save
-  end
 }
 
