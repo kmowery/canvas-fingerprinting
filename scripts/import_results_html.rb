fb1d81ad5bc27b5a46a2e12c1e8f22267526b870
kmowery <kmowery@gmail.com>
2012-02-25 18:15:08 -0800

    Import script now supports multiple files at a time

--- scripts/import_results_html.rb
+++ scripts/import_results_html.rb
@@ -11,53 +11,56 @@ if ARGV.length == 0 then
   exit
 end
 
-html = Nokogiri::HTML(open(ARGV[0]))
 
-rows = []
+ARGV.each {|filename|
+  html = Nokogiri::HTML(open(filename))
 
-#entries = html.css('div#header').each {|x|
-#entries = html.xpath('//span[starts-with(@id, "long_answer")]').each {|x|
-html.css('tr').each {|row|
+  rows = []
 
-  row.css('span[id^="answer_long"]').each {|span|
+  #entries = html.css('div#header').each {|x|
+  #entries = html.xpath('//span[starts-with(@id, "long_answer")]').each {|x|
+  html.css('tr').each {|row|
 
-    # If we find an answer in this row, check for links that are IDs
-    userid = row.css("a").first.children.first.to_s.strip
-    assignmentid = (/'.*'.*'(.*)'/.match row.css("a").first.attribute("onclick")).captures[0]
+    row.css('span[id^="answer_long"]').each {|span|
 
-    # Why is there a <dl>? I don't know.
-    dl = span.children[0]
+      # If we find an answer in this row, check for links that are IDs
+      userid = row.css("a").first.children.first.to_s.strip
+      assignmentid = (/'.*'.*'(.*)'/.match row.css("a").first.attribute("onclick")).captures[0]
 
-    row = {}
-    dl.css("dt").each {|c|
-      row[c.inner_html.gsub(":","")] = c.next_sibling.next_sibling.inner_html
-    }
+      # Why is there a <dl>? I don't know.
+      dl = span.children[0]
 
-    s = Sample.new
-    s.useragent = row["useragent"]
-    s.userinput = row["input"]
-    s.webglrenderer = row["renderer"]
-    s.webglversion = row["webglversion"]
-    s.webglvendor = row["webglvendor"]
-    s.save
+      row = {}
+      dl.css("dt").each {|c|
+        row[c.inner_html.gsub(":","")] = c.next_sibling.next_sibling.inner_html
+      }
 
-    puts "Browser unclear: " + s.useragent if s.browser.nil?
-    puts "OS unclear: " + s.useragent if s.os.nil?
+      s = Sample.new
+      s.useragent = row["useragent"]
+      s.userinput = row["input"]
+      s.webglrenderer = row["renderer"]
+      s.webglversion = row["webglversion"]
+      s.webglvendor = row["webglvendor"]
+      s.save
 
-    row.keys.select{ |k|  k =~ /exp-/ }.sort.each do |experiment_name|
-      exp = Experiment.where(:name => experiment_name.gsub("exp-","")).first
-      c     = Canvas.new
+      puts "Browser unclear: " + s.useragent if s.browser.nil?
+      puts "OS unclear: " + s.useragent if s.os.nil?
 
-      c    .experiment_id = exp.id
-      c    .sample_id = s.id
-      c    .png = row[experiment_name]
+      row.keys.select{ |k|  k =~ /exp-/ }.sort.each do |experiment_name|
+        exp = Experiment.where(:name => experiment_name.gsub("exp-","")).first
+        c     = Canvas.new
 
-      if /exp-webgl-teapot/ =~ experiment_name and c.png != "no webgl" then
-        puts "./grantBonus.sh -workerid #{userid} -amount 0.10 -reason \"Your browser supports WebGL\" -assignment #{assignmentid}"
+        c    .experiment_id = exp.id
+        c    .sample_id = s.id
+        c    .png = row[experiment_name]
+
+        if /exp-webgl-teapot/ =~ experiment_name and c.png != "no webgl" then
+          puts "./grantBonus.sh -workerid #{userid} -amount 0.10 -reason \"Your browser supports WebGL\" -assignment #{assignmentid}"
+        end
+
+        c.save
       end
-
-      c.save
-    end
+    }
   }
 }
 
