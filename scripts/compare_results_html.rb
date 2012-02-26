c0d810cfa49ad4f0a5bfcca3076c3c32ee89e897
kmowery <kmowery@gmail.com>
2012-02-25 21:52:26 -0800

    Added a script to compare Amazon results to existing DB samples

---
+++ scripts/compare_results_html.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'nokogiri'
+require 'pp'
+
+require_relative '../model.rb'
+
+if ARGV.length == 0 then
+  puts "I need a filename"
+  exit
+end
+
+
+ARGV.each {|filename|
+  html = Nokogiri::HTML(open(filename))
+
+  rows = []
+
+  #entries = html.css('div#header').each {|x|
+  #entries = html.xpath('//span[starts-with(@id, "long_answer")]').each {|x|
+  html.css('tr').each {|row|
+
+    row.css('span[id^="answer_long"]').each {|span|
+
+      # If we find an answer in this row, check for links that are IDs
+      userid = row.css("a").first.children.first.to_s.strip
+      assignmentid = (/'.*'.*'(.*)'/.match row.css("a").first.attribute("onclick")).captures[0]
+
+
+      # Why is there a <dl>? I don't know.
+      dl = span.children[0]
+
+      row = {}
+      dl.css("dt").each {|c|
+        row[c.inner_html.gsub(":","")] = c.next_sibling.next_sibling.inner_html
+      }
+
+      s = Sample.where(:userid => userid).first
+
+      next if s.nil?
+
+      pp "Duplicate: #{s.userid}"
+
+      row.keys.select{ |k|  k =~ /exp-/ }.sort.each do |experiment_name|
+        exp = Experiment.where(:name => experiment_name.gsub("exp-","")).first
+
+        c = Canvas.where(:experiment_id => exp.id, :sample_id => s.id).first
+
+        if c.png != row[experiment_name] then
+          pp "HERE WE GO"
+        end
+      end
+    }
+  }
+}
+
